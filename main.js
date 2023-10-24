8bf089c4fa9fa5280071a838ea08b86127a91d67
MUKILTEO\HerreraSturmDL <herrerasturmdl@mukilteo.wednet.edu>
2023-10-23 18:29:56 -0700

    added more to the main page

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    switchTab('learn-tab', 'math');
+}, false);
+
 function sendEmail(subjectInput, bodyInput) {
     let subjectText = document.getElementById(subjectInput).value;
     let bodyText = document.getElementById(bodyInput).value;
 
     window.open("mailto:herrerasturmdl@mukilteo.wednet.edu?subject=" + subjectText + "&body=" + bodyText);
+}
+
+function switchTab(contentId, tabId) {
+    document.getElementById('math').style.display = 'none';
+    document.getElementById('art-design').style.display = 'none';
+    document.getElementById('c-sharp').style.display = 'none';
+    document.getElementById('unity').style.display = 'none';
+    document.getElementById('soft-skills').style.display = 'none';
+
+    document.getElementById(tabId).style.display = 'block';
 }
