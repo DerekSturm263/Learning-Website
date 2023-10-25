f7c09f37847a47aae41138480b91d993b741f8d9
DerekSturm263 <dereksturm263@gmail.com>
2023-10-25 07:07:10 -0700

    y

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     switchTab('learn-tab', 'math');
+    switchTab('home-tabs', 'daily-slides');
 }, false);
 
 function sendEmail(subjectInput, bodyInput) {
@@ -10,11 +11,23 @@ function sendEmail(subjectInput, bodyInput) {
 }
 
 function switchTab(contentId, tabId) {
-    document.getElementById('math').style.display = 'none';
-    document.getElementById('art-design').style.display = 'none';
-    document.getElementById('c-sharp').style.display = 'none';
-    document.getElementById('unity').style.display = 'none';
-    document.getElementById('soft-skills').style.display = 'none';
+    if (contentId == 'learn-tab')
+    {
+        document.getElementById('math').style.display = 'none';
+        document.getElementById('art-design').style.display = 'none';
+        document.getElementById('c-sharp').style.display = 'none';
+        document.getElementById('unity').style.display = 'none';
+        document.getElementById('soft-skills').style.display = 'none';
+    }
+    else
+    {
+        document.getElementById('daily-slides').style.display = 'none';
+        document.getElementById('links').style.display = 'none';
+        document.getElementById('calendar').style.display = 'none';
+        document.getElementById('learn').style.display = 'none';
+        document.getElementById('syllabus').style.display = 'none';
+        document.getElementById('contact-me').style.display = 'none';
+    }
 
     document.getElementById(tabId).style.display = 'block';
 }
